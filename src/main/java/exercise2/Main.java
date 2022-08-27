b2d272a22a3fee0d25f7d36b273b7920aeb88525
Nirsch95 <castromanzano95@gmail.com>
2022-08-27 16:06:28 -0500

    Taller # 4 - Excepciones
    Adiciono ejercicio 2

---
+++ src/main/java/exercise2/Main.java
@@ -0,0 +1,13 @@
+package exercise2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                ¿Es posible ejecutar un programa en java que contenga varias clases con métodos main? 
+                En caso positivo, ¿cómo se determina el punto de entrada a un programa?
+                """);
+        System.out.println("""
+                Efectivamente es posible tener varias clases con metodo main, En el caso de IntelliJ idea tiene por defecto la opción de correr el archivo en el cual se encuentra posicionado y también se puede dandole click derecho a la clase y ejecutar
+                """);
+    }
+}
---
+++ src/main/java/exercise2/Main2.java
@@ -0,0 +1,7 @@
+package exercise2;
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Soy otra clase con metodo main");
+    }
+}
